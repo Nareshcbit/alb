40e2754f2eac6f3f1f021b4dd72676eac05e1ed2
Nareshcbit <naresh.johnny@gmail.com>
2024-07-15 14:18:39 -0400

    Add listener rules

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,12 @@ output "alb_dns_name" {
   value       = aws_lb.this.dns_name
 }
 
-output "alb_target_group_arn" {
-  description = "The ARN of the target group"
-  value       = aws_lb_target_group.this.arn
+output "alb_target_group_arns" {
+  description = "The ARNs of the target groups"
+  value       = aws_lb_target_group.this[*].arn
+}
+
+output "alb_listener_arns" {
+  description = "The ARNs of the listeners"
+  value       = aws_lb_listener.this[*].arn
 }
