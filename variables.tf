a32da0d5ecc956be2c6782a68719f38aa5f2ff8d
Nareshcbit <naresh.johnny@gmail.com>
2024-07-15 11:40:52 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,113 @@
+variable "name" {
+  description = "The name of the ALB"
+  type        = string
+}
+
+variable "internal" {
+  description = "If true, the ALB will be internal"
+  type        = bool
+  default     = false
+}
+
+variable "load_balancer_type" {
+  description = "The type of load balancer. Either 'application' or 'network'"
+  type        = string
+  default     = "application"
+}
+
+variable "security_groups" {
+  description = "The security groups to attach to the ALB"
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = "The subnets to attach to the ALB"
+  type        = list(string)
+}
+
+variable "enable_deletion_protection" {
+  description = "If true, deletion protection will be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "target_group_name" {
+  description = "The name of the target group"
+  type        = string
+}
+
+variable "target_group_port" {
+  description = "The port for the target group"
+  type        = number
+  default     = 80
+}
+
+variable "target_group_protocol" {
+  description = "The protocol for the target group"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+}
+
+variable "target_type" {
+  description = "The type of targets for the target group (instance, ip, lambda, alb)"
+  type        = string
+  default     = "instance"
+}
+
+variable "health_check_interval" {
+  description = "The interval for health checks"
+  type        = number
+  default     = 30
+}
+
+variable "health_check_path" {
+  description = "The path for health checks"
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_protocol" {
+  description = "The protocol for health checks"
+  type        = string
+  default     = "HTTP"
+}
+
+variable "health_check_timeout" {
+  description = "The timeout for health checks"
+  type        = number
+  default     = 5
+}
+
+variable "healthy_threshold" {
+  description = "The number of healthy checks before considering the target healthy"
+  type        = number
+  default     = 3
+}
+
+variable "unhealthy_threshold" {
+  description = "The number of unhealthy checks before considering the target unhealthy"
+  type        = number
+  default     = 3
+}
+
+variable "ssl_policy" {
+  description = "The SSL policy for HTTPS listeners"
+  type        = string
+  default     = "ELBSecurityPolicy-2016-08"
+}
+
+variable "certificate_arn" {
+  description = "The ARN of the certificate for HTTPS listeners"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
