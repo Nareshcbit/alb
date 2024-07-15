e83fefc6e37c36481e26400095f2fc5f9f63c347
Nareshcbit <naresh.johnny@gmail.com>
2024-07-15 11:44:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "this" {
   port        = var.target_group_port
   protocol    = var.target_group_protocol
   vpc_id      = var.vpc_id
-  target_type = var.target_type
+  target_type = "ip"
 
   health_check {
     interval            = var.health_check_interval
@@ -34,19 +34,6 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
   
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = var.ssl_policy
-  certificate_arn   = var.certificate_arn
-
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
